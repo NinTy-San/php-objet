d8d14a29f2ce945d37d48795253179b08ae30e0a
NinTy-San <arnaud.dohou@lepoles.com>
2018-11-05 16:54:35 +0100

    1er jour php Orienté objet
    01- classe, objet et instance
    02- getteur, setteur et constructeur
    03- constante, static et self

--- 03-constante-static-self/maison.php
+++ 03-constante-static-self/maison.php
@@ -4,6 +4,8 @@ class maison
     public static $espaceTerrain = '500m²'; // appartient à la classe
     public $couleur = 'blanc'; // appartient à l'objet
 
+    const HAUTEUR = '10m'; // appartient à la class
+
     private static $nbPiece = 7; // appartient à la classe
     private  $nbPorte = 10; // appartient à l'objet
 
@@ -22,11 +24,12 @@ $maison1 = new Maison;
 echo 'Couleur : ' . $maison1->couleur . '<hr>'; // J'appele une propriété de mon objet par mon objet
 echo 'Nombre de porte : ' . $maison1->getnbPorte() . '<hr>'; // J'appele une méthode de mon objet par mon objet
 
+echo 'hauteur : ' Maison::
+
 // ---------------------
 
 // echo $maison1->espaceTerrain; // Erreur,on ne peut pas appeler une propriété static par mon objet (->)
 // echo Maison::$couleur; // Erreur, on ne peut pas appeler une propriété public avec ma class (::)
 // echo Maison::getnbPorte(); // Erreur, on ne peut pas appeler une méthode de mon objet avec ma classe
 // echo $maison1->getNbPiece(); // pas d'erreur..., mais ça devrait car la méthode est static et donc il faudrait l'appeler par la classe et non pa l'objet
-
-echo $maison1::$espaceTerrain; //  devrait donner là aussi une erreur, c'est n'importe quoi !! A banir !
+// echo $maison1::$espaceTerrain; //  devrait donner là aussi une erreur, c'est n'importe quoi !! A banir !
